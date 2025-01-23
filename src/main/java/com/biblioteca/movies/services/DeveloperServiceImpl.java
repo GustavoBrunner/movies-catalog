ea7bafa8957a6cea97c0a526aee5ca5507f5f5df
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-01-23 18:06:26 -0300

    Feat: Generic service and developer service abstraction and implementation created

--- src/main/java/com/biblioteca/movies/services/DeveloperServiceImpl.java
+++ src/main/java/com/biblioteca/movies/services/DeveloperServiceImpl.java
@@ -1,13 +1,16 @@
 package com.biblioteca.movies.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.biblioteca.movies.domain.Developer;
+import com.biblioteca.movies.dtos.DeveloperDTO;
+import com.biblioteca.movies.dtos.DeveloperViewDTO;
 import com.biblioteca.movies.repositories.DeveloperRespository;
 import com.biblioteca.movies.services.contracts.DeveloperService;
+import com.biblioteca.movies.util.DeveloperToDeveloperDto;
+import com.biblioteca.movies.util.contracts.DtoMapper;
 
 public class DeveloperServiceImpl implements DeveloperService {
 
@@ -19,8 +22,9 @@ public class DeveloperServiceImpl implements DeveloperService {
     }
 
     @Override
-    public Optional<Developer> findById(Long id) {
-        return respository.findById(id);
+    public DeveloperViewDTO findById(Long id) {
+        Developer entity = respository.findById(id).get(); 
+        return mapViewDto(entity, new DeveloperToDeveloperDto());
     }
 
     @Override
@@ -29,19 +33,39 @@ public class DeveloperServiceImpl implements DeveloperService {
     }
 
     @Override
-    public Developer create(Developer entity) {
-        return respository.saveAndFlush(entity);
+    public DeveloperViewDTO create(DeveloperDTO entity) {
+        Developer newEntity = mapEntity(entity, new DeveloperToDeveloperDto());
+        respository.saveAndFlush(newEntity);
+        return mapViewDto(newEntity, new DeveloperToDeveloperDto());
     }
 
     @Override
-    public Developer update(Developer entity) {
-        return respository.saveAndFlush(entity);
+    public DeveloperViewDTO update(DeveloperDTO entity) {
+        Developer newEntity = mapEntity(entity, new DeveloperToDeveloperDto());
+        respository.saveAndFlush(newEntity);
+        return mapViewDto(newEntity, new DeveloperToDeveloperDto());
     }
 
     @Override
-    public Developer delete(Long id) {
-        Developer dev = findById(id).get();
+    public DeveloperViewDTO delete(Long id) {
+        Developer dev = respository.findById(id).get();
         respository.delete(dev);
-        return dev;
+        return mapViewDto(dev, new DeveloperToDeveloperDto());
+    }
+
+
+    @Override
+    public DeveloperDTO mapDto(Developer entity, DtoMapper<Developer, DeveloperDTO, DeveloperViewDTO> mapper) {
+        return mapper.mapToDto(entity);
+    }
+
+    @Override
+    public Developer mapEntity(DeveloperDTO dto, DtoMapper<Developer, DeveloperDTO, DeveloperViewDTO> mapper) {
+        return mapper.mapToEntity(dto);
+    }
+
+    @Override
+    public DeveloperViewDTO mapViewDto(Developer entity, DtoMapper<Developer, DeveloperDTO, DeveloperViewDTO> mapper) {
+        return mapper.mapToViewDto(entity);
     }
 }
--- src/main/java/com/biblioteca/movies/services/contracts/GenericService.java
+++ src/main/java/com/biblioteca/movies/services/contracts/GenericService.java
@@ -1,13 +1,18 @@
 package com.biblioteca.movies.services.contracts;
 
 import java.util.List;
-import java.util.Optional;
+import com.biblioteca.movies.util.contracts.DtoMapper;
 
 
-public interface GenericService<T> {
-    public Optional<T> findById(Long id);
+public interface GenericService<T, T1,T2> {
+    public T2 findById(Long id);
     public List<T> findAll();
-    public T create(T entity);
-    public T update(T entity);
-    public T delete(Long id);
+    public T2 create(T1 entity);
+    public T2 update(T1 entity);
+    public T2 delete(Long id);
+
+    public T1 mapDto(T entity, DtoMapper<T,T1,T2> mapper);
+    public T mapEntity(T1 dto, DtoMapper<T,T1,T2> mapper);
+    public T2 mapViewDto(T entity, DtoMapper<T,T1,T2> mapper);
+
 }
