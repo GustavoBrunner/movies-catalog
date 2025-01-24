baf3ffaff5549b29db576e83120cc70d9b77a38b
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-01-24 13:00:40 -0300

    feat: list mapper method and return types changed to view dto

--- src/main/java/com/biblioteca/movies/services/contracts/GenericService.java
+++ src/main/java/com/biblioteca/movies/services/contracts/GenericService.java
@@ -6,7 +6,7 @@ import com.biblioteca.movies.util.contracts.DtoMapper;
 
 public interface GenericService<T, T1,T2> {
     public T2 findById(Long id);
-    public List<T> findAll();
+    public List<T2> findAll();
     public T2 create(T1 entity);
     public T2 update(T1 entity);
     public T2 delete(Long id);
@@ -14,5 +14,6 @@ public interface GenericService<T, T1,T2> {
     public T1 mapDto(T entity, DtoMapper<T,T1,T2> mapper);
     public T mapEntity(T1 dto, DtoMapper<T,T1,T2> mapper);
     public T2 mapViewDto(T entity, DtoMapper<T,T1,T2> mapper);
+    public List<T2> mapToListDto(List<T> entities, DtoMapper<T,T1,T2> mapper);
 
 }
