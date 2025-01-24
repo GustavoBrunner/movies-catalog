0afc9e5a05c391e8cdf7ab2181dd227904d37b21
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-01-24 18:08:29 -0300

    config: Open feign annotation added

--- src/main/java/com/biblioteca/movies/MoviesApplication.java
+++ src/main/java/com/biblioteca/movies/MoviesApplication.java
@@ -2,9 +2,11 @@ package com.biblioteca.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
+@EnableFeignClients
 public class MoviesApplication {
 
 	public static void main(String[] args) {
