b40aaa255d151e98a974617375009bf1660aa2b1
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-01-24 18:10:11 -0300

    feat: open feign config made, added apikey for the request

---
+++ src/main/java/com/biblioteca/movies/infra/FeignClientConfig.java
@@ -0,0 +1,21 @@
+package com.biblioteca.movies.infra;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.RequestInterceptor;
+
+@Configuration
+public class FeignClientConfig {
+
+    @Value("${omdb.apikey}")
+    private String apiKey;
+
+    @Bean
+    public RequestInterceptor requestInterceptor(){
+        return requestTemplate -> {
+            requestTemplate.query("apikey", apiKey);
+        };
+    }
+}
