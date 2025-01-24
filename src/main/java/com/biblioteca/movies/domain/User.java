55e1ec461f03376d9650a0fbe7c8e82b13860f03
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-01-24 12:57:36 -0300

    update: new attributes inserted in user entity Feat: user types with different rules

--- src/main/java/com/biblioteca/movies/domain/User.java
+++ src/main/java/com/biblioteca/movies/domain/User.java
@@ -1,14 +1,19 @@
 package com.biblioteca.movies.domain;
 
+import java.time.LocalDateTime;
+
 import com.biblioteca.movies.dtos.UserDTO;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,20 +28,39 @@ import lombok.NoArgsConstructor;
 public class User {
     public User(UserDTO dto){
         this.id = dto.id();
-        this.name =dto.name();
+        this.userName =dto.userName();
         this.email = dto.email();
         this.password = dto.password();
+        this.userType = dto.userType();
+        this.movieQtd = dto.movieQtd();
     }
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    protected Long id;
+    @Column(name = "id_user")
+    private Long id;
 
-    @Column(nullable = true)
-    protected String email;
+    @Column(nullable = false)
+    private String email;
     
-    @Column(nullable = true)
-    protected String password;
+    @Column(nullable = false)
+    private String password;
     
-    @Column(nullable = true)
-    protected String name;
+
+    @Column(name = "username", nullable = false)
+    private String userName;
+
+    @Column(name = "movie_qtd")
+    private Integer movieQtd;
+
+    @Column(name = "token")
+    private String confirmationToken;
+
+    @Column(name = "token_datetime")
+    private LocalDateTime tokenExpirationTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserType userType;
+
 }
