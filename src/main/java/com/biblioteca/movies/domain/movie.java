2e68e7611776f482a5e7539e29bfd8875ece7991
Gustavo Brunner <92188787+GustavoBrunner@users.noreply.github.com>
2025-01-24 18:07:31 -0300

    feat: movie entity

--- src/main/java/com/biblioteca/movies/domain/movie.java
+++ src/main/java/com/biblioteca/movies/domain/movie.java
@@ -1,19 +1,58 @@
 package com.biblioteca.movies.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity(name = "movie")
+@Table(name = "movie")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Movie {
 
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "imdb_id")
+    private String imbdId;
 
     private String name;
+
+    private String title;
+
+    private String year;
+    
+    private String released;
+
+    private String runtime;
+
+    private String genre;
+
+    private String director;
+
+    private String writer;
+
+    private String actors;
+
+    private String plot;
+
+    private String language;
+
+    private String country;
+
+    private String awards;
+
+    private String poster;
+
+    @Column(name = "imdb_rating")
+    private String imdbRating;
+
 }
